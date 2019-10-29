fea862a3154be55c05317616a2f53fc77dc43d98
Tony <tony@gmail.com>
2019-10-29 21:06:36 +0800

    未传入令牌值时直接退出程序。

--- app/src/main/java/com/sunnyweather/android/MainActivity.kt
+++ app/src/main/java/com/sunnyweather/android/MainActivity.kt
@@ -2,6 +2,7 @@ package com.sunnyweather.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.lifecycle.liveData
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
@@ -11,5 +12,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (SunnyWeatherApplication.TOKEN.isEmpty()) {
+            Toast.makeText(this, "请在SunnyWeatherApplication中填入你申请到的令牌值", Toast.LENGTH_LONG).show()
+            finish()
+        }
     }
 }
